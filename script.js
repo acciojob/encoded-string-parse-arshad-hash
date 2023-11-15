dca64b7d6b20c5350aba2811c527baadbb5cfc49
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-15 22:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const parseCode = (str) => {
-  // your code here
-};
-
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+function parseCode(str){
+let red =/0+/g;
+let yell=str.split(red);
+console.log(str);
+console.log(yell);
+let ans={
+   firstname: null,
+   lastName:null,
+   id: null
+};  
+yell.forEach( (a,i)=>{
+ let key = Object.keys(ans)[i];
+ ans[key]=yell[i];
+});
+return (ans);
+   };
+  
+//   // Do not change the code below
+  const str = prompt("Enter str: ");
+  alert(JSON.stringify(parseCode(str)));
